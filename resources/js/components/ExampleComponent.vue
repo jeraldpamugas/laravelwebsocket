7efa2fcc945f2655f90cfae40565ccea7a615d77
Jerald T. Pamugas <jtpamugas@virginiafood.com.ph>
2020-12-11 16:05:01 +0800

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Active accounts</div>
+
+                    <div class="card-body">
+                        <ul>
+                            <li v-for="user in users">
+                                {{ user.name }}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                users: null
+            }
+        },
+        mounted() {
+            Echo.join('users')
+            .here((users) => {
+                this.users = users
+            })
+            .joining((user) => {
+                this.users.push(user)
+            })
+            .leaving((user) => {
+                this.users.splice(this.users.indexOf(user), 1)
+            })
+        }
+    }
+</script>
